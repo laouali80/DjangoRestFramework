0bc615d8784f66172ef4d764720431647aecaece
Laouali Bachir <laoualibachir2000@gmail.com>
2024-05-17 08:42:27 +0100

    change routes name

--- api/apitest/urls.py
+++ api/apitest/urls.py
@@ -4,9 +4,9 @@ from . import views
 
 urlpatterns = [
     path('users/', views.usersInfo, name='users'),
-    path('user/<int:id>/', views.userDetail, name='user-detail'),
-    path('create-user/', views.userCreate, name='create-user'),
-    path('update-user/<int:id>/', views.userUpdate, name='update-user'),
+    path('users/<int:id>/', views.userDetail, name='user-detail'),
+    path('create/', views.userCreate, name='create-user'),
+    path('users/<int:id>/', views.userUpdate, name='update-user'),
     # path('update-user/', views.userUpdate, name='update-user'),
-    path('delete-user/<int:id>/', views.userDelete, name='delete-user'),
+    path('user/<int:id>/', views.userDelete, name='delete-user'),
 ]
